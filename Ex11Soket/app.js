023291a56c9a7d1292b45c66d83d2618876a4b27
이선아 <130373132+LeeSeona-dev@users.noreply.github.com>
2023-07-18 14:09:13 +0900

    Create app.js

---
+++ Ex11Soket/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const indexRouter = require('./routes')
+const webSocket = require('./socket')
+const app = express()
+
+app.set('views', __dirname+'/views')
+app.set('view engine' , 'html')
+
+nunjucks.configure('views',{
+    express: app,
+    watch:true
+})
+
+app.use('/',indexRouter)
+
+app.set('port', process.env.PORT||8888)
+
+const server = app.listen(app.get('port'),()=>{
+    console.log(app.get('port'),'번 포트에서 서버 연결 기다리는 중...')
+})
+
+webSocket(server)
